f5ff86681a189b509d80f183f7dd42193dbaf9f8
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-10-01 14:13:49 -0400

    added paths and middleware to server.js

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const app = express();
+
+//middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//assign routes to server
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//listener
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}!`);
+});
